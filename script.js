185df32ada5a7c386fdb2372d914f35fcc020ff1
CaroHerrera6 <cherreraluciaro@gmail.com>
2025-07-08 13:33:15 -0300

    Second commit

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const SHOW_HIDE = document.querySelectorAll(".question");
 
 SHOW_HIDE.forEach((answer) => {
   answer.addEventListener("click", () => {
+    // const FRST_BTN = answer.parentElement.querySelector(".answer-text-first");
     const ANS_BTN = answer.parentElement.querySelector(".answer-text");
     const IMG_BTN = answer.querySelector(".icon");
     const IS_VISIBLE = getComputedStyle(ANS_BTN).display === "block";
+    // const IS_IT_THERE = getComputedStyle(FRST_BTN).display === "block";
 
     //toggle visibility
 
     ANS_BTN.style.display = IS_VISIBLE ? "none" : "block";
+    // FRST_BTN.style.display = IS_IT_THERE ? "block" : "none";
 
     //toggle icon
 
